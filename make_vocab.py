8ddeeb4a18a0eec37b57472ac421ce3fc91e1897
Bryce <bryce.h.bernstein@gmail.com>
2024-11-19 19:23:48 -0600

    Added the ability to make indexed vocabulary.

---
+++ make_vocab.py
@@ -0,0 +1,11 @@
+def index_words(words):
+    vocab = {'<pad>': 1}
+    index = 2
+    for word in words:
+        if word not in vocab:
+            vocab[word] = index
+            index += 1
+    return vocab
+
+def inverse_vocab(vocab):
+    return {index: word for word, index in vocab.items()}
