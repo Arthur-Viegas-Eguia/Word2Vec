d2606ab5c9682198f6a49956f7546bb35af57213
Arthur Viegas Eguia <95381448+Arthur-Viegas-Eguia@users.noreply.github.com>
2024-11-24 15:29:35 -0600

    TensorFlow

--- word2vec.py
+++ word2vec.py
@@ -1,123 +1,200 @@
+# -*- coding: utf-8 -*-
+
+
+import io
+import re
+import string
+import tqdm
+
 import numpy as np
-from vocabulary import Vocab
+from numpy.linalg import norm
+
+import tensorflow as tf
+from tensorflow.keras import layers
 import math
-from multiprocessing import Pool
 
-class Word2Vec:
-    def __init__(self, dim_size, vocab: Vocab, unigram_frequency_weight, num_ns, window_size, learning_rate, compute_loss):
-        '''Initializes word2vec model with dim_size dimensions where each word has a normally distributed standard vector'''
-        #Initialize word embeddings
-        self.target_embeddings = np.random.uniform(-.01, .01, size=(len(vocab.vocab), dim_size))
-        self.context_embeddings = np.random.uniform(-.01, .01, size=(len(vocab.vocab), dim_size))
+SEED = 69
+AUTOTUNE = tf.data.AUTOTUNE
 
-        # the word frequency table used for negative sampling
-        self.word_frequency = (vocab.word_counts ** unigram_frequency_weight) / np.sum(vocab.word_counts ** unigram_frequency_weight)
+# Generates skip-gram pairs with negative sampling for a list of sequences
+# (int-encoded sentences) based on window size, number of negative samples
+# and vocabulary size.
+def generate_training_data(sequences, window_size, num_ns, vocab_size, seed):
+  # Elements of each training example are appended to these lists.
+  targets, contexts, labels = [], [], []
 
-        # various things that must be set up.
-        self.vocab = vocab
-        self.dim_size = dim_size
-        self.num_ns = num_ns
-        self.window_size = window_size
-        self.learning_rate = learning_rate
-        self.compute_loss = compute_loss
+  # Build the sampling table for `vocab_size` tokens.
+  sampling_table = tf.keras.preprocessing.sequence.make_sampling_table(vocab_size)
 
-    def normalize_embeds(self):
-        '''Scales all vectors down so that the largest vector is magnitude 1. This is useful for a stable loss function while also having relative position saved.'''
-        '''This sets the magnitude of all vectors to 1'''
-        norms = np.linalg.norm(self.target_embeddings, axis=1, keepdims=True)
-        self.target_embeddings /= norms
-        norms = np.linalg.norm(self.context_embeddings, axis=1, keepdims=True)
-        self.context_embeddings /= norms
+  # Iterate over all sequences (sentences) in the dataset.
+  for sequence in tqdm.tqdm(sequences):
 
-    def probability(self, target_emb, context_emb):
-        '''Computes the probability that word_1 and word_2 occur in context using the sigmoid of their dot product'''
-        return 1 / (1 + np.exp(-np.dot(target_emb, context_emb)))
+    # Generate positive skip-gram pairs for a sequence (sentence).
+    positive_skip_grams, _ = tf.keras.preprocessing.sequence.skipgrams(
+          sequence,
+          vocabulary_size=vocab_size,
+          sampling_table=sampling_table,
+          window_size=window_size,
+          negative_samples=0)
 
-    def make_training_data(self, words):
-        '''Generates training data for an input corpus represented by an array of word indices'''
-        samples = []
-        for i in range(len(words)):
-            for j in range(max(0, i - self.window_size), min(len(words), i + self.window_size + 1)):
-                if i == j:
-                    continue
-                sample = np.zeros((2 + self.num_ns,), dtype=int)
-                sample[0] = words[i]
-                sample[1] = words[j]
-                samples.append(self.make_negative_sample(sample))
-        return np.array(samples)
+    # Iterate over each positive skip-gram pair to produce training examples
+    # with a positive context word and negative samples.
+    for target_word, context_word in positive_skip_grams:
+      context_class = tf.expand_dims(
+          tf.constant([context_word], dtype="int64"), 1)
+      negative_sampling_candidates, _, _ = tf.random.log_uniform_candidate_sampler(
+          true_classes=context_class,
+          num_true=1,
+          num_sampled=num_ns,
+          unique=True,
+          range_max=vocab_size,
+          seed=seed,
+          name="negative_sampling")
 
-    def make_negative_sample(self, sample):
-        '''Creates negative samples for an input sample. This samples from word_frequency. We allow all negative samples except the target word.'''
-        indices = np.arange(len(self.vocab.vocab))
-        sample[2:] = np.random.choice(indices, size=self.num_ns, p=self.word_frequency, replace=False)
-        for i in range(2, len(sample)):
-            while sample[i] == sample[0]:
-                sample[i] = np.random.choice(indices, p=self.word_frequency)
-                break
-        return sample
-    
-    def compute_average_loss(self, samples):
-        tar_norms = np.linalg.norm(self.target_embeddings, axis=1)
-        con_norms = np.linalg.norm(self.context_embeddings, axis=1)
-        max_norm = max(np.max(tar_norms), np.max(con_norms))
-        tar_embeddings = self.target_embeddings / max_norm
-        con_embeddings = self.context_embeddings / max_norm
-        targets = tar_embeddings[samples[:,0]]
-        contexts = con_embeddings[samples[:,1:]]
-        total_loss = 0
-        for i in range(len(targets)):
-            loss = math.log(self.probability(targets[i], contexts[i][0]))
-            for j in range(1, len(contexts[i])):
-                loss += math.log(1 - self.probability(targets[i], contexts[i][j]))
-            total_loss -= loss
-        return total_loss / len(samples)
-    
-    def train(self, samples, epochs):
-        '''
-        Passes over all samples epochs times, computing gradients along the way. Will print average losses if self.compute_loss is true.
-        '''
-        suffix = '' if not self.compute_loss else f' Average Loss: {self.compute_average_loss(samples)}'
-        print(f'Starting Training.{suffix}')
-        for i in range(1, epochs + 1):
-            print(f'Starting Epoch {i}...')
-            np.random.shuffle(samples) # shuffling is important so that we aren't always computing the same gradients in the same order
-            # self.normalize_embeds()
-            self.gradient_descent(samples, self.learning_rate)
-            suffix = '' if not self.compute_loss else f' Average Loss: {self.compute_average_loss(samples)}'
-            print(f'Epoch {i} Complete.{suffix}')
-    
-    def gradient_descent(self, samples, learning_rate):
-        '''Computes gradients for each word with respect for each sample'''
-        for sample in samples:
-            target_emb = self.target_embeddings[sample[0]]
-            context_embs = self.context_embeddings[sample[1:]]
+      # Build context and label vectors (for one target word)
+      context = tf.concat([tf.squeeze(context_class,1), negative_sampling_candidates], 0)
+      label = tf.constant([1] + [0]*num_ns, dtype="int64")
 
-            # update for target embedding
-            target_update = (self.probability(target_emb, context_embs[0]) - 1) * context_embs[0]
-            for j in range(1, len(context_embs)):
-                target_update += self.probability(target_emb, context_embs[j]) * context_embs[j]
-            self.target_embeddings[sample[0]] -= target_update * learning_rate
+      # Append each element from the training example to global lists.
+      targets.append(target_word)
+      contexts.append(context)
+      labels.append(label)
 
-            # update for positive context embedding
-            pos_context_update = (self.probability(target_emb, context_embs[0]) - 1) * target_emb
-            self.context_embeddings[sample[1]] -= pos_context_update * learning_rate
-            
-            # update negative context embeddings
-            for j in range(1, len(context_embs)):
-                neg_context_update = self.probability(target_emb, context_embs[j]) * target_emb
-                self.context_embeddings[sample[j + 1]] -= neg_context_update * learning_rate
+  return targets, contexts, labels
 
-    def get_target_embeddings(self):
-        return {self.vocab.inv_vocab[i]: self.target_embeddings[i] for i in range(len(self.target_embeddings))}
-    
-    def get_context_embeddings(self):
-        return {self.vocab.inv_vocab[i]: self.context_embeddings[i] for i in range(len(self.target_embeddings))}
-
-    def get_joint_embeddings(self):
-        return {self.vocab.inv_vocab[i]: self.target_embeddings[i] + self.context_embeddings[i] for i in range(len(self.target_embeddings))}
+path_to_file = "/content/output_movies.txt"
+text_ds = tf.data.TextLineDataset(path_to_file).filter(lambda x: tf.cast(tf.strings.length(x), bool))
+# Now, create a custom standardization function to lowercase the text and
+# remove punctuation.
+def custom_standardization(input_data):
+  lowercase = tf.strings.lower(input_data)
+  return tf.strings.regex_replace(lowercase,
+                                  '[%s]' % re.escape(string.punctuation), '')
 
 
-if __name__ == '__main__':
-    v = Vocab([['im', 'so', 'mad', 'right', 'now']])
-    model = Word2Vec(5, v, window_size=2, num_ns=4)
-    print(model.make_training_data([4, 1, 2, 5, 3, 6, 4, 3, 4]))
+# Define the vocabulary size and the number of words in a sequence.
+vocab_size = 8096
+sequence_length = 2
+
+# Use the `TextVectorization` layer to normalize, split, and map strings to
+# integers. Set the `output_sequence_length` length to pad all samples to the
+# same length.
+vectorize_layer = layers.TextVectorization(
+    standardize=custom_standardization,
+    max_tokens=vocab_size,
+    output_mode='int',
+    output_sequence_length=sequence_length)
+vectorize_layer.adapt(text_ds.batch(1024))
+# Save the created vocabulary for reference.
+inverse_vocab = vectorize_layer.get_vocabulary()
+print(inverse_vocab[:20])
+# Vectorize the data in text_ds.
+text_vector_ds = text_ds.batch(1024).prefetch(AUTOTUNE).map(vectorize_layer).unbatch()
+sequences = list(text_vector_ds.as_numpy_iterator())
+print(len(sequences))
+for seq in sequences[:5]:
+  print(f"{seq} => {[inverse_vocab[i] for i in seq]}")
+  targets, contexts, labels = generate_training_data(
+    sequences=sequences,
+    window_size=5,
+    num_ns=7,
+    vocab_size=vocab_size,
+    seed=SEED)
+
+targets = np.array(targets)
+contexts = np.array(contexts)
+labels = np.array(labels)
+
+print('\n')
+print(f"targets.shape: {targets.shape}")
+print(f"contexts.shape: {contexts.shape}")
+print(f"labels.shape: {labels.shape}")
+
+BATCH_SIZE = 1024
+BUFFER_SIZE = 10000
+dataset = tf.data.Dataset.from_tensor_slices(((targets, contexts), labels))
+dataset = dataset.shuffle(BUFFER_SIZE).batch(BATCH_SIZE, drop_remainder=True)
+print(dataset)
+
+class Word2Vec(tf.keras.Model):
+  def __init__(self, vocab_size, embedding_dim):
+    super(Word2Vec, self).__init__()
+    self.target_embedding = layers.Embedding(vocab_size,
+                                      embedding_dim,
+                                      name="w2v_embedding")
+    self.context_embedding = layers.Embedding(vocab_size,
+                                       embedding_dim)
+
+  def call(self, pair):
+    target, context = pair
+    # target: (batch, dummy?)  # The dummy axis doesn't exist in TF2.7+
+    # context: (batch, context)
+    if len(target.shape) == 2:
+      target = tf.squeeze(target, axis=1)
+    # target: (batch,)
+    word_emb = self.target_embedding(target)
+    # word_emb: (batch, embed)
+    context_emb = self.context_embedding(context)
+    # context_emb: (batch, context, embed)
+    dots = tf.einsum('be,bce->bc', word_emb, context_emb)
+    # dots: (batch, context)
+    return dots
+
+def custom_loss(x_logit, y_true):
+      return tf.nn.sigmoid_cross_entropy_with_logits(logits=x_logit, labels=y_true)
+embedding_dim = 128
+word2vec = Word2Vec(vocab_size, embedding_dim)
+word2vec.compile(optimizer='adam',
+                 loss=custom_loss,
+                 metrics=['accuracy'])
+tensorboard_callback = tf.keras.callbacks.TensorBoard(log_dir="logs")
+word2vec.fit(dataset, epochs=20, callbacks=[tensorboard_callback])
+
+weights = word2vec.get_layer('w2v_embedding').get_weights()[0]
+vocab = vectorize_layer.get_vocabulary()
+out_v = io.open('vectors.tsv', 'w', encoding='utf-8')
+out_m = io.open('metadata.tsv', 'w', encoding='utf-8')
+for index, word in enumerate(vocab):
+  if index == 0:
+    continue  # skip 0, it's padding.
+  vec = weights[index]
+  out_v.write('\t'.join([str(x) for x in vec]) + "\n")
+  out_m.write(word + "\n")
+out_v.close()
+out_m.close()
+
+word_weight_tuple = []
+for i in range(len(vocab)):
+  word_weight_tuple.append((vocab[i], weights[i]))
+
+def get_word_index(word, word_weight_tuple):
+  for i in range(len(word_weight_tuple)):
+    if word == word_weight_tuple[i][0]:
+      return i
+  return -1
+
+def cos_sim(embedding1, embedding2):
+  cosine = np.dot(embedding1,embedding2)/(norm(embedding1)*norm(embedding2))
+  return cosine
+
+def get_distance_among_words(index, word_weight_tuple):
+  target_tuple = word_weight_tuple[index]
+  print(index, target_tuple[0])
+  word_cosine_similarities = []
+  i = 0
+  for word_weight in word_weight_tuple:
+    if i < 2:
+      i += 1
+      continue
+    word_cosine_similarities.append((word_weight[0], math.exp(cos_sim(target_tuple[1], word_weight[1]))))
+
+
+  words_in_order = sorted(word_cosine_similarities, key = lambda x: -x[1])
+
+  return words_in_order[1:51]
+
+
+def get_closest_words(word):
+  return get_distance_among_words(get_word_index(word, word_weight_tuple), word_weight_tuple)
+
+print(get_closest_words("finzell"))
