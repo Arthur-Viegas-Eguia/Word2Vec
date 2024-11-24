c3e22a6155147adf36d8b83102a73ee5d80ec728
James Carlson <therealjamescarlson@gmail.com>
2024-11-24 17:26:47 -0600

    Changed most_similar to not delete embeddings

--- most_similar.py
+++ most_similar.py
@@ -1,49 +1,54 @@
 from cosine_similarity import cos_sim
-import numpy as np
+import heapq
 # The goal here is to take an input word and create a list of the n most similar words (cos sim)
 
 def most_similar_by_word(target, vocab, topn):
     #1.Find the embedding of word, then calculate the cosine similarity between it and every word in the vocab.
     #1b.As you go, make a dict of tuples, where index 0 is the word and index 1 is the cosine similarity to target.
     #2.Find the highest cos sim in the dict, then remove that sim's tuple from the dict and append it to a list. Repeat step 2 topn times.
-
+    already_used = []
     target_embed = vocab[target]
+    already_used.append(target)
     all_sims = {}
     topn_similar = []
-    store = vocab[target]
-    del vocab[target]
     for word in vocab:
-        all_sims[word] = cos_sim(target_embed, vocab[word])
+        if word != target:
+            all_sims[word] = cos_sim(target_embed, vocab[word])
+            '''
     for x in range(topn):
         most_similar = max(all_sims, key=lambda key: all_sims[key])
         topn_similar.append(most_similar)
         del all_sims[most_similar]
-    vocab[target] = store
-    return topn_similar
+        '''
+    topn_similar_vals = heapq.nlargest(topn, all_sims.values(), key=all_sims.get)
+    for n in topn_similar_vals:
+        topn_similar.append(n + ": " + all_sims[n])
+    string = str("The most similar words to " + target + 'are: ' + topn_similar)
+    
+    return string
 
 def most_similar(positive, negative, vocab, topn):
     # expected that positive will be a list of words (strings), same with negatives. Vocab will be a dictionary with all words as keys
     # and their embeddings as values. topn is the size of the list of similar words.
-    sample_vec = vocab['bad']
-    pos_sum = np.zeros(sample_vec.size)
-    neg_sum = np.zeros(sample_vec.size)
+    pos_sum = 0
+    neg_sum = 0
     key_vectors = 0
     for vector in positive:
-        pos_sum += vocab[vector]
+        pos_sum += vector
         key_vectors += 1
     for vector in negative:
-        neg_sum += vocab[vector]
+        neg_sum += vector
         key_vectors += 1
-    avg_vec = (pos_sum - neg_sum) / key_vectors
+    avg_vec = pos_sum - neg_sum / key_vectors
     all_sims = {}
     topn_similar = []
     for word in vocab:
         all_sims[word] = cos_sim(avg_vec, vocab[word])
-    for x in range(topn):
-        most_similar = max(all_sims, key=lambda key: all_sims[key])
-        topn_similar.append(most_similar)
-        del all_sims[most_similar]
-    return topn_similar
+    topn_similar_vals = heapq.nlargest(topn, all_sims.values(), key=all_sims.get)
+    for n in topn_similar_vals:
+        topn_similar.append(n + ": " + all_sims[n])
+    string = str("The most similar words are:" + topn_similar)
+    return string
     
 def similarity(key1, key2):
     return cos_sim(key1, key2)
@@ -64,3 +69,14 @@ def closer_than(key1, key2, vocab):
     for word in vocab:
         if cos_sim(key1, word) > baseline:
             closer_list.append(word, ": ", cos_sim(key1, word))
+
+if __name__ == '__main__':
+    test_dict = {"blarg" : 10, "scrungle": 18, "gungk" : 4, "fzzzbzzz" : 16, "bababooey" : 5}
+    list = []
+    topn_similar_vals = heapq.nlargest(2, test_dict.values(), key=test_dict.get)
+    for n in topn_similar_vals:
+        list.append(n + ": " + test_dict[n])
+    string = str("The most similar words to " + "squaw" + 'are: ' + list)
+    print(string)
+
+
